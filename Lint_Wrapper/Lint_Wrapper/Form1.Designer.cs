1a502b9931f61c9d8c44ca6d0d4780dc297bded9
robert-delwood <87502826+robert-delwood@users.noreply.github.com>
2024-09-21 18:22:10 -0500

    Add files via upload

---
+++ Lint_Wrapper/Lint_Wrapper/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Lint_Wrapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Evaluate target file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(147, 699);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(150, 170);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(833, 526);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(150, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(557, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoEllipsis = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(150, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "(No items found)";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 170);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(132, 526);
+            this.panel1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 721);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Version: 0.9";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(823, 702);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Copy table to clipboard";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(713, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Open target file...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(150, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "OpenAPI source file name:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(150, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Ruleset file name:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(150, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(557, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(851, 25);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Save target file name";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(713, 77);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(132, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Open ruleset file...";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button6.Location = new System.Drawing.Point(851, 77);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(132, 23);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "Save ruleset file name";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 742);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1500, 975);
+            this.MinimumSize = new System.Drawing.Size(500, 325);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Lint Wrapper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Lint_Wrapper/Lint_Wrapper/Form1.cs
@@ -0,0 +1,349 @@
+﻿//E:\Development\member_experience_June_21_2021.yaml
+
+
+// https://www.dreamincode.net/forums/topic/153776-running-command-line-commands-from-winform/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lint_Wrapper
+{
+    public partial class Form1 : Form
+    {
+        List<BrokenRules> gBrokenRules = new List<BrokenRules>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Can also be configured through property settings
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToOrderColumns = false;
+            dataGridView1.AllowUserToResizeColumns = true;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+
+            // Get settings strings
+            textBox2.Text = Properties.Settings.Default.rulesFile;
+            textBox1.Text = Properties.Settings.Default.targetFile;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            //Boolean didRun = RunCommandLine(false);
+            //if(didRun == true) SetDefaultCheckBox();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileName = textBox1.Text.Trim();
+            if (fileName.Length == 0 || File.Exists(fileName) == false)
+            {
+                MessageBox.Show(string.Format("The following file could not be found:\r\n\r\n{0}", fileName), "File Not Found", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            string theRuleset = textBox2.Text.Trim();
+            if (fileName.Length == 0 || File.Exists(theRuleset) == false)
+            {
+                MessageBox.Show(string.Format("The following file could not be found:\r\n\r\n{0}", theRuleset), "File Not Found", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            gBrokenRules.Clear();
+            DeleteRadioButtons();
+
+            Boolean didRun = RunCommandLine(true);
+            if (didRun == true)
+            {
+                SetDefaultCheckBox();
+            }
+        }
+
+        private void DeleteRadioButtons()
+        {
+            panel1.Controls.Clear();
+        }
+
+        private Boolean RunCommandLine(Boolean showFileNotFound)
+        {
+            //Process.Start("cmd.exe", @"/C ping www.google.com > e:\development\log.log");
+
+            string missingFiles = string.Empty;
+
+            string fileName = textBox1.Text.Trim();
+            if (fileName.Length == 0 || File.Exists(fileName) == false)
+            {
+                if (showFileNotFound == true) missingFiles += string.Format("{0}\r\n", fileName);
+            }
+
+            string theRuleset = textBox2.Text.Trim();
+            if (theRuleset.Length == 0 || File.Exists(theRuleset) == false)
+            {
+                if (showFileNotFound == true) missingFiles += string.Format("{0}\r\n", theRuleset);
+            }
+
+            if(missingFiles.Trim().Length != 0)
+            {
+                MessageBox.Show(string.Format("The following file(s) could not be found:\r\n\r\n{0}", missingFiles), "File Not Found", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+
+            //Set up the command line command
+            string theCommand = @"spectral lint";
+            string arguments = string.Format("/C {0} --ruleset {1} {2}", theCommand, theRuleset, fileName);
+
+            //Capture the output to a string for this application.
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            //p.StartInfo.Arguments = @"/C spectral lint --ruleset e:\development\spectral.yaml e:\development\swagger_petstore.yaml";
+            p.StartInfo.Arguments = arguments;
+
+            //p.StartInfo.Arguments = @"vale Accounts.txt";
+
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.UseShellExecute = false;
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            if (output.Length == 0)
+            {
+                MessageBox.Show("It looks like the command did not run properly\r\n\r\nPlease make the command is correct and try again.", "Command Did Not Run", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+
+            dataGridView1.DataSource = null;
+
+            string[] subs = output.Split('\n');
+            List<string> myList = new List<string>(subs);
+            myList.RemoveAll(x => x.Length == 0);
+
+            myList.RemoveAt(0);
+
+            label2.Text = myList[myList.Count - 1].Substring(4);
+            myList.RemoveAt(myList.Count - 1);
+
+            RegexOptions options = RegexOptions.None;
+            Regex regex = new Regex("[ ]{2,}", options);
+            for (int i = 0; i < myList.Count; i++)
+            {
+                myList[i] = regex.Replace(myList[i].Trim(), "#");
+
+                string[] newItem = myList[i].Split('#');
+                //var x = newItem[4] != null ? newItem[4] : null;
+
+                var certainValue = newItem.ElementAtOrDefault(4) != null ? newItem[4] : "(Not found.)";
+
+                gBrokenRules.Add(new BrokenRules(newItem[0], newItem[1], newItem[2], newItem[3], certainValue));
+            }
+
+            var rulesList = gBrokenRules.Select(x => x.ruleName).Distinct();
+            int theLocation = 0;
+            int startPoint = 10;
+            int startLeftEdge = 10;
+
+            theLocation = CreateRadioButton(theLocation, "Show all", startLeftEdge, startPoint, true, gBrokenRules.Count);
+
+            var listCount = gBrokenRules.Where(x => x.warningLevel.ToLower() == "error".ToLower()).ToList().Count;
+            theLocation = CreateRadioButton(theLocation, "Errors only", startLeftEdge, startPoint, false, listCount);
+
+            listCount = gBrokenRules.Where(x => x.warningLevel.ToLower() == "warning".ToLower()).ToList().Count;
+            theLocation = CreateRadioButton(theLocation, "Warnings only", startLeftEdge, startPoint, false, listCount);
+            theLocation++;
+
+            foreach (var item in rulesList)
+            {
+                listCount = gBrokenRules.Where(x => x.ruleName.ToLower() == item).ToList().Count;
+                theLocation = CreateRadioButton(theLocation, item, startLeftEdge, startPoint, false, listCount);
+            }
+
+            var gridStartPoint = CalculateStringLengthPixels();
+            dataGridView1.Left = gridStartPoint + 5;
+            dataGridView1.Width = this.Width - dataGridView1.Left - 30;
+            panel1.Width = gridStartPoint - 10;
+            label2.Left = dataGridView1.Left;
+            label3.Left = dataGridView1.Left;
+
+            Application.DoEvents();
+            return true;
+        }
+
+        private int CalculateStringLengthPixels()
+        {
+            int gridStartPoint = -1;
+            foreach (var item in panel1.Controls)
+            {
+                //var y = ((Control)item).Text;
+
+                using (System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(new Bitmap(1, 1)))
+                {
+                    SizeF size = graphics.MeasureString(((Control)item).Text, new Font("Segoe UI", 11, FontStyle.Regular, GraphicsUnit.Point));
+                    if (size.Width > gridStartPoint) gridStartPoint = (int)size.Width;
+                }
+            }
+
+            return gridStartPoint;
+        }
+
+        private int CreateRadioButton(int theLocation, string item, int startLeftEdge, int startPoint, Boolean isChecked, int TheCount)
+        {
+            RadioButton box;
+
+            box = new RadioButton();
+            box.Text = string.Format("{0} ({1})", item, TheCount);
+            box.Tag = item;
+            box.Checked = isChecked;
+            box.AutoSize = true;
+            box.Location = new Point(startLeftEdge, theLocation++ * 20 + startPoint); //vertical //box.Location = new Point(i * 50, 10); //horizontal
+            box.CheckedChanged += new EventHandler(radioButton_CheckedChanged);
+
+            panel1.Controls.Add(box);
+
+            return theLocation;
+        }
+
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton box = (RadioButton)sender;
+
+            Application.DoEvents();
+
+            dataGridView1.DataSource = null;
+            if (box.Checked == true)
+            {
+                if (box.Tag.ToString() == "Show all")
+                {
+                    dataGridView1.DataSource = gBrokenRules;
+                }
+                else if (box.Tag.ToString() == "Errors only")
+                {
+                    dataGridView1.DataSource = gBrokenRules.Where(x => x.warningLevel.ToLower() == "error".ToLower()).ToList();
+                }
+                else if (box.Tag.ToString() == "Warnings only")
+                {
+                    dataGridView1.DataSource = gBrokenRules.Where(x => x.warningLevel.ToLower() == "warning".ToLower()).ToList();
+                }
+                else
+                {
+                    dataGridView1.DataSource = gBrokenRules.Where(x => x.ruleName.ToLower() == box.Tag.ToString().ToLower()).ToList();
+                }
+
+                label3.Text = string.Format("Items in list: {0}", dataGridView1.Rows.Count);
+            }
+        }
+
+        private void SetDefaultCheckBox()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = gBrokenRules;
+            label3.Text = string.Format("Items in list: {0}", dataGridView1.Rows.Count);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string collectedString = string.Empty;
+            List<BrokenRules> myBrokenRules = (List<BrokenRules>)dataGridView1.DataSource;
+            if (myBrokenRules == null || myBrokenRules.Count == 0)
+            {
+                MessageBox.Show("No results found.\r\n\r\nNothing was copied to the clipboard.", "No results found", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            foreach (var item in myBrokenRules)
+            {
+                collectedString += string.Format("{0}\t{1}\t{2}\t{3}\t{4}\r\n", item.lineNumber, item.warningLevel, item.ruleName, item.description, item.paths);
+            }
+
+            Clipboard.SetText(collectedString);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var dialogReturn = OpenFile();
+            if (dialogReturn == string.Empty) return;
+
+            if (dialogReturn.ToLower() != textBox1.Text.ToLower().Trim()) dataGridView1.DataSource = null;
+
+            textBox1.Text = dialogReturn;
+        }
+
+        private string OpenFile()
+        {
+            string selectedFileName = string.Empty;
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"E:\XXDevelopment",
+                Title = "Browse Text Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "yaml",
+                Filter = "YAML files (*.yaml, *.yml)|*.yaml; *.yml|txt files (*.txt)|*.txt|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) selectedFileName = openFileDialog1.FileName;
+
+            return selectedFileName.Trim();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.targetFile = textBox1.Text.Trim();
+            Properties.Settings.Default.Save();
+        }
+    }
+
+    public class BrokenRules
+    {
+        //The DisplayName is used for column headings in a datagridview.
+        [DisplayName("The line location (line:column)")]
+        public string lineNumber { get; set; }
+
+        [DisplayName("The warning level")]
+        public string warningLevel { get; set; }
+
+        [DisplayName("The rule name")]
+        public string ruleName { get; set; }
+
+        [DisplayName("The rule description")]
+        public string description { get; set; }
+
+        [DisplayName("The violation path")]
+        public string paths { get; set; }
+
+        //Constructor
+        public BrokenRules(string item1, string item2, string item3, string item4, string item5)
+        {
+            this.lineNumber = item1;
+            this.warningLevel = item2;
+            this.ruleName = item3;
+            this.description = item4;
+            this.paths = item5;
+        }
+    }
+}
